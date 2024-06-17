8bce0348abcb7a31ce4a84c23d524b6e1e4cf0f8
Lars Aaberg <lars@aaberg.cc>
2024-06-17 15:46:19 +0200

    Initial commit. Contains only the code and setup created by spring initializr

---
+++ src/main/java/aaberg/htmx_demo/HtmxDemoApplication.java
@@ -0,0 +1,13 @@
+package aaberg.htmx_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmxDemoApplication.class, args);
+	}
+
+}
